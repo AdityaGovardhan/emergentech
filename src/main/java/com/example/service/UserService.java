f3b8fa0f2e2a5f613b293028a7032068aa29b5fa
Arihant94 <sinhaarihant@gmail.com>
2020-02-22 20:51:26 -0700

    Model done

---
+++ src/main/java/com/example/service/UserService.java
@@ -0,0 +1,39 @@
+package com.example.service;
+import java.util.HashMap;
+import java.util.ArrayList;
+import com.example.relationaldataaccess.User;
+import com.example.relationaldataaccess.UserEvent;
+import com.example.relationaldataaccess.Group;
+
+@Component
+public class UserService {
+
+    @Autowired
+    EntityManager entityManager;
+
+    @Autowired
+    UserRepository userRepository;
+
+    public User findByUserID(String emailId) {
+//        String sql = "Select e from " + User.class.getName() + " e "
+//                + " Where e.userName = :userName ";
+//
+//        Query query = entityManager.createQuery(sql, User.class);
+//        query.setParameter("username", username);
+        return user;
+    }
+
+    public HashMap<User, ArrayList<Tag>> findUserInterests(User user){
+        ArrayList<Group> groups = user.groupsEnrolled;
+        ArrayList<Tag> tags = new ArrayList<Tag>;
+
+        for(int i=0, i<groups.size(), i++){
+            tags.addALL(groups.get(i).tags)
+        }
+
+        System.out.println(tags)
+
+        HashMap<User, ArrayList<Tag>> userProfile = new HashMap<String, ArrayList<Tag>>();
+        return userProfile;
+    }
+}
