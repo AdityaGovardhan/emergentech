d2730a13fe66dc551f78f0b62d22f22c02da272a
Suryavamsi Tenneti <suryavamsi8@gmail.com>
2020-02-23 00:15:16 -0700

    Module imports

--- my-social-app-UI/src/app/app.module.ts
+++ my-social-app-UI/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatNativeDateModule} from '@angular/material/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {DemoMaterialModule} from './material-module';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -32,7 +33,7 @@ import {DemoMaterialModule} from './material-module';
     ReactiveFormsModule,
     MatNativeDateModule,
     FormsModule,
-    
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
