669a978eae0753bded009308e7ef63c174ce682a
Aditya Govardhan <Aditya.Govardhan@cirrus.com>
2020-02-22 17:22:51 -0700

    sample code modified

--- src/main/java/com/michaelcgood/dao/SystemRepository.java
+++ src/main/java/com/michaelcgood/dao/SystemRepository.java
@@ -1,11 +1,7 @@
 package com.michaelcgood.dao;
-
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import com.michaelcgood.model.SystemExample;
-
+import com.michaelcgood.model.System;
 @Repository
-public interface SystemRepository extends CrudRepository<SystemExample,Long> {
-	
-
+public interface SystemRepository extends CrudRepository<System,Long> {
 }
