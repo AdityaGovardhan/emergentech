55bbe235b4d76b7c060d99a47a50604cd019ab6b
Suryavamsi Tenneti <suryavamsi8@gmail.com>
2020-02-23 00:13:45 -0700

    Credits validation functionality added

--- my-social-app-UI/src/app/events/events.component.ts
+++ my-social-app-UI/src/app/events/events.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import{EVENTS} from "../mock/mock-events";
 import{USERS} from "../mock/mock-user";
 import {MatFormFieldModule} from '@angular/material/form-field';
+import { NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-events',
@@ -14,7 +16,11 @@ export class EventsComponent implements OnInit{
     registered: boolean;
     event_name : string;
     user_name : string;
-    is_validated : boolean;
+    event_validated : boolean;
+    event_id: string;
+    user_id : string;
+    event_credits:string;
+    private REST_API_SERVER = "http://localhost:3000";
 
     ngOnInit(): void {
       this.event_name = EVENTS.event_name;
@@ -22,15 +28,34 @@ export class EventsComponent implements OnInit{
       this.event_description = EVENTS.event_description;
       this.registered = EVENTS.event_registered;
       this.user_name = "John";
+      this.user_id = "123";
+      this.event_id = EVENTS.event_id;
       for(var user of USERS){
         if(user.user_name == this.user_name){
           for(var event of user.events){
             // console.log("Event",event);
             if(event.event_name == this.event_name){
-              this.is_validated = event.validated;
+              this.event_validated = event.validated;
             }
           }
         }
       }
     }
+
+    is_validated(): boolean{
+      return this.event_validated;
+    }
+
+    validate(f: NgForm): void{
+      this.event_validated = true;
+      console.log(f);
+      var user_details = {
+        'user_id':this.user_id,
+        'event_id': this.event_id,
+        'validated': this.event_validated
+      }
+      //return user_details to backend.
+    }
+
+
 }
